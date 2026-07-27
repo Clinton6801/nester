9b240ba7a69f61715eb8f7c5668e90f04eff2731
Pascal <opascal221@gmail.com>
2026-07-28 00:31:16 +0100

    test(api): add unit tests for YieldHarvest model (#962)
    Co-authored-by: opascal221-design <opascal221-design@users.noreply.github.com>

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -181,6 +181,9 @@ func run() error {
 
 	userRepository := postgres.NewUserRepository(db)
 	userService := service.NewUserService(userRepository)
+	if accountCipher != nil {
+		userService.WithCipher(accountCipher)
+	}
 	userHandler := handler.NewUserHandler(userService)
 	userVaultsSvc := service.NewUserVaultsService(vaultRepository)
 	userHandler.SetUserVaultsService(userVaultsSvc)
